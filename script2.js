2539a32fd65af7f422d1e19be0457293d404f10c
Cornelia van der Schilden <72788749+CorneliavanderSchilden@users.noreply.github.com>
2021-01-09 17:35:07 +0100

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,41 @@
+//opdracht 2
+
+//Arrays 1
+let rainbowColors = [];
+console.log(rainbowColors);
+
+//Arrays 2-3
+let rainbowColors = ['red', 'yellow', 'purple'];
+console.log(rainbowColors);
+
+//Arrays 4
+let rainbowColors = ['red', 'yellow', 'purple'];
+console.log(rainbowColors.length);
+
+//Arrays 5
+let rainbowColors = ['red', 'yellow', 'purple', 'green'];
+console.log(rainbowColors[0]);
+
+//Array 6
+
+let rainbowColors = ['red', 'yellow', 'purple', 'green'];
+console.log(rainbowColors[rainbowColors.lenght - 1]);
+
+//Arrays 7
+let rainbowColors = ['red', 'yellow', 'purple'];
+const count = rainbowColors.push('green');
+console.log(rainbowColors);
+
+//Arrays 8
+let rainbowColors = ['red', 'yellow', 'purple', 'green'];
+const count = rainbowColors.push(5);
+console.log(rainbowColors);
+
+//Arrays 9
+let rainbowColors = ['red', 'yellow', 'purple', 'green'];
+const count = rainbowColors.push("greeting:Hi, ik ben een object");
+console.log(rainbowColors);
+
+//Arrays 10
+let rainbowColors = ['red', 'yellow', 'purple', 'green', "greeting:Hi, ik ben een object"];
+console.log(rainbowColors[4]);
---
+++ script3.js
@@ -0,0 +1,21 @@
+//opdracht 3
+
+// Deel 3 vraag 1
+
+console.log("Naam kat 3:", catBreeds[2].name)
+
+//Deel 3 vraag 2
+
+console.log("Energie level kat 1:", catBreeds[0].energy_level)
+
+//Deel 3 vraag 3
+
+console.log("Eerste temperament kat 2:", catBreeds[1].temperament[0])
+
+//Deel 3 vraag 4
+
+console.log("Laatste temperament kat 3:", catBreeds[2].temperament[4])
+
+//Deel 3 vraag 5
+
+console.log("Favoriete voedsel kat 3:", catBreeds[2].favourite_food)
